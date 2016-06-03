6cb2b2744bea16bdb2b2fbd90fd7b679381e7721
EMacAdie <EMacAdie@gmail.com>
2016-06-03 00:46:39 -0500

    Done with koans

--- clojure-koans/src/koans/24_macros.clj
+++ clojure-koans/src/koans/24_macros.clj
@@ -14,16 +14,17 @@
 
 (defmacro r-infix [form]
   (cond (not (seq? form))
-        __
+        form
         (= 1 (count form))
         `(r-infix ~(first form))
         :else
         (let [operator (second form)
               first-arg (first form)
-              others __]
+              others ((comp rest rest) form)]
           `(~operator
             (r-infix ~first-arg)
             (r-infix ~others)))))
+
 
 (meditations
   "Macros are like functions created at compile time"
