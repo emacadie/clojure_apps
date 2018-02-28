361924d15b7434bf1a2c91dbea76655699fde355
Eric MacAdie <eric@MacAdie.net>
2018-02-27 23:23:39 -0600

    Adding propertied and stuff for db

--- twitter-retriever/src/twitter_retriever/cline.clj
+++ twitter-retriever/src/twitter_retriever/cline.clj
@@ -1,5 +1,7 @@
 (ns twitter-retriever.cline
   (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojurewerkz.propertied.properties :as p])
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
 (def cli-options
@@ -10,9 +12,9 @@
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
    ;; A non-idempotent option
    ["-v" nil "Verbosity level"
-    :id :verbosity
-    :default 0
-    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+     :id :verbosity
+     :default 0
+     :assoc-fn (fn [m k _] (update-in m [k] inc))]
    ["-f" "--file-path FILE_PATH" "File path"
     :id :file-path]
    ;; A boolean option defaulting to nil
@@ -20,6 +22,17 @@
 
 (defn -main [& args]
   (println "In cline")
-  (def something(parse-opts args cli-options))
-  (println "here is something:", something))
+  (def something (parse-opts args cli-options))
+  (println "here is something:", something)
+  (println "here is options: ", (:options something))
+  (println "here is file-path: ",  (:file-path (:options something)))
+  (println "here is file-path: ",  (class (:file-path (:options something))))
+  (def prop-path (:file-path (:options something)))
+  (println "Here is prop-path: ", prop-path)
+  (def the-prop (p/load-from (io/file prop-path)))
+  (println "Here is the prop: ", the-prop)
+  (println "the-prop is a ", (class the-prop))
+  (println "what happens if we conver to a map?", (class (p/properties->map the-prop true)))
+  ;; perhaps the threading macro can help
+  (println "Here it is as a map: ", (p/properties->map the-prop true)))
 
