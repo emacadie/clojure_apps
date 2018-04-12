9844a3fbf45a1bad6463beab7ff01ed2e4a6fd73
Eric MacAdie <eric@MacAdie.net>
2018-04-12 00:07:21 -0500

    Done with chapter 3

--- brave-clojure/src/brave_clojure/chapter03.clj
+++ brave-clojure/src/brave_clojure/chapter03.clj
@@ -28,30 +28,24 @@
 
 ;; 5. Create a function that's similar to symmetrize-body-parts except that it has to work with weird space aliens with radial symmetry. 
 ;; Instead of two eyes, arms, legs, and so on, they have five.
+;; extra parts: {:name "neck" :size 2}, {:name "left-shoulder" :size 3}
+;; {:name "left-upper-arm" :size 3} {:name "chest" :size 10}
+;; {:name "back" :size 10} {:name "left-forearm" :size 3}
+;; {:name "abdomen" :size 6}  {:name "left-kidney" :size 1}
+;; {:name "left-hand" :size 2} {:name "left-knee" :size 2}
+;; {:name "left-thigh" :size 4} {:name "left-lower-leg" :size 3}
+;; {:name "left-achilles" :size 1} {:name "left-foot" :size 2}
+
 (def asym-hobbit-body-parts [{:name "head" :size 3}
                              {:name "left-eye" :size 1}
                              {:name "left-ear" :size 1}
                              {:name "mouth" :size 1}
-                             {:name "nose" :size 1}
-                             {:name "neck" :size 2}
-                             {:name "left-shoulder" :size 3}
-                             {:name "left-upper-arm" :size 3}
-                             {:name "chest" :size 10}
-                             {:name "back" :size 10}
-                             {:name "left-forearm" :size 3}
-                             {:name "abdomen" :size 6}
-                             {:name "left-kidney" :size 1}
-                             {:name "left-hand" :size 2}
-                             {:name "left-knee" :size 2}
-                             {:name "left-thigh" :size 4}
-                             {:name "left-lower-leg" :size 3}
-                             {:name "left-achilles" :size 1}
-                             {:name "left-foot" :size 2}])
+                             ])
 
-
+;; add a parameter for other parts
 (defn matching-part
-  [part]
-  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+  [part other]
+  {:name (clojure.string/replace (:name part) #"^left-" other)
    :size (:size part)})
 
 (defn symmetrize-body-parts
@@ -65,5 +59,42 @@
         (recur remaining 
                (into final-body-parts
                      (set [part (matching-part part)])))))))
+
+;; make a call to the other part
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts other]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part 
+                                         (matching-part part "right-") 
+                                         (matching-part part other)])))
+          []
+          asym-body-parts))
+
+
+(symmetrize-body-parts asym-hobbit-body-parts )
+(better-symmetrize-body-parts asym-hobbit-body-parts "other-")
+
+;; 6. Create a function that generalizes symmetrize-body-parts and the function you created in Exercise 5. 
+;; The new function should take a collection of body parts and the number of matching body parts to add. 
+;; If you’re completely new to Lisp languages and functional programming, it probably won’t be obvious how to do this. 
+;; If you get stuck, just move on to the next chapter and revisit the problem later.
+;; this kind of sort of works
+(defn varied-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts other-vec]
+  (set (flatten (reduce (fn [final-body-parts part]
+                      (into final-body-parts (set [part
+                                                   (for [other-part other-vec]
+                                                     (matching-part part other-part) )
+                                                   ])))
+                    []
+                    asym-body-parts))))
+
+(varied-symmetrize-body-parts asym-hobbit-body-parts ["up-", "down-", "front-"])
+(def part "left-elbow")
+(def other-vec ["up-", "down-", "front-"])
+(doseq [other-part other-vec]
+  (matching-part part other-part) )
 
 
