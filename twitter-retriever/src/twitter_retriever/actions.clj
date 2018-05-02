7be713970127c2d5b979bb0dd7a1388f08e35ad0
Eric MacAdie <eric@MacAdie.net>
2018-05-01 22:13:28 -0500

    Incorporating some suggestions from code review

--- twitter-retriever/src/twitter_retriever/actions.clj
+++ twitter-retriever/src/twitter_retriever/actions.clj
@@ -14,7 +14,7 @@
 
 (defn get-tweet-map [my-oauth-creds params-map]
   "max-id comes in as a map with a key of :min"
-  (statuses-user-timeline :oauth-creds my-oauth-creds :params params-map ))
+  (:body (statuses-user-timeline :oauth-creds my-oauth-creds :params params-map )))
 
 (defn create-user [user-name my-oauth-creds batch-time]
   (def user-map (users-show :oauth-creds my-oauth-creds :params {:screen-name user-name}))
@@ -22,16 +22,16 @@
   (rdbms/call-insert-user (:body user-map))
   (def max-id (get-in user-map [ :body :status :id ]))
   
-  (def tweet-map (get-tweet-map my-oauth-creds {:screen-name user-name                                                    
+  (def tweet-map-body (get-tweet-map my-oauth-creds {:screen-name user-name                                                    
                                                 :max_id max-id  
                                                 :count 200
                                                 :include_rts false
                                                 :tweet_mode "extended"}))
   
-  (def map-body (:body tweet-map))
+  ; (def map-body (:body tweet-map))
   (println "Here is max-id: ", max-id)
 
-  (loop [the-map-body map-body]
+  (loop [the-map-body tweet-map-body]
     (println "in loop, here is count of the-map-body: ", (count the-map-body))
     ;; would "when" be better here?
     (if (> (count the-map-body) 0)
@@ -45,12 +45,12 @@
         
         (println "Here is next-max-id: ", (rdbms/get-min-tweet-id {:screen_name user-name}))
         (def next-max-id (:min (rdbms/get-min-tweet-id {:screen_name user-name})))
-        (recur (:body (get-tweet-map my-oauth-creds 
-                                     {:screen-name user-name                                                    
-                                      :max_id (dec next-max-id) 
-                                      :count 200
-                                      :include_rts false
-                                      :tweet_mode "extended"})))))))
+        (recur (get-tweet-map my-oauth-creds 
+                              {:screen-name user-name                                                    
+                               :max_id (dec next-max-id) 
+                               :count 200
+                               :include_rts false
+                               :tweet_mode "extended"}))))))
 
 (defn get-with-default [arg-map the-key default-value]
   (def result (get arg-map the-key))
@@ -61,15 +61,15 @@
 (defn insert-more-tweets [user-name my-oauth-creds batch-time]
   (def starting-tweet-id (:max (rdbms/get-max-tweet-id {:screen_name user-name})))
   (println "Here is starting-tweet-id: ", starting-tweet-id)
-  (def tweet-map (get-tweet-map  my-oauth-creds 
+  (def tweet-map-body (get-tweet-map  my-oauth-creds 
                                  {:screen-name user-name                                                    
                                   :since_id starting-tweet-id  
                                   :count 10
                                   :include_rts false
                                   :tweet_mode "extended"}))
-  (def map-body (:body tweet-map))
+  ; (def map-body (:body tweet-map))
 
-    (loop [the-map-body map-body]
+    (loop [the-map-body tweet-map-body]
     (println "in loop, here is count of the-map-body: ", (count the-map-body))
     ;; would "when" be better than "if" here?
     (when (> (count the-map-body) 0)
@@ -84,11 +84,11 @@
         (println "Here is next-since-id: ", (rdbms/get-max-tweet-id {:screen_name user-name}))
         ; (def next-since-id (:max (rdbms/get-max-tweet-id {:screen_name user-name})))
         (def next-max-id (rdbms/get-min-tweet-id-for-batch {:screen_name user-name, :batch_time batch-time}))
-        (recur (:body (get-tweet-map my-oauth-creds {:screen-name user-name                                                    
-                                                     :since_id starting-tweet-id  
-                                                     :count 10
-                                                     :max_id (dec (:min next-max-id))
-                                                     :include_rts false
-                                                     :tweet_mode "extended"}))))))
+        (recur (get-tweet-map my-oauth-creds {:screen-name user-name                                                    
+                                              :since_id starting-tweet-id  
+                                              :count 10
+                                              :max_id (dec (:min next-max-id))
+                                              :include_rts false
+                                              :tweet_mode "extended"})))))
   (println "Done inserting"))
 
--- twitter-retriever/src/twitter_retriever/work.clj
+++ twitter-retriever/src/twitter_retriever/work.clj
@@ -13,12 +13,7 @@
 
 (def cli-options
   ;; An option with a required argument
-  [["-a" "--action ACTION" "An action to be performed: --create-user or --retrieve-tweets"
-    :id :action
-    ; :parse-fn #(Integer/parseInt %)
-    ; :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]
-    ]
-   ["-u" "--user USER" "Name of twitter user's tweets to get"
+  [["-u" "--user USER" "Name of twitter user's tweets to get"
     :id :user
     ;:validate [#(> (count %) 10) "String must be at least 10 chars (like ending with .properties)"]
     ]
@@ -28,35 +23,38 @@
 
 (defn -main [& args]
   (println "In work")
-  (def arg-map (parse-opts args cli-options))
-  (println "here is arg-map:", arg-map)
-  (println "here is options: ", (:options arg-map))
-  (println "here is action: ",  (:action (:options arg-map)))
-  (def batch-time (timec/to-timestamp (timel/local-now)))
   
-  (def user-name (:user (:options arg-map)))
-  ;; put some sort of checking in here
+  (let [arg-map (parse-opts args cli-options)
+        batch-time (timec/to-timestamp (timel/local-now))
+        user-name (:user (:options arg-map))]
+    (println "here is arg-map:", arg-map)
+    (println "here is options: ", (:options arg-map))
+    ; (def )
+  
+    ; (def user-name (:user (:options arg-map)))
+    ;; put some sort of checking in here
 
-  (def database-url (env :database-url))
+    (def database-url (env :database-url))
  
-  (def twitter-auth (rdbms/get-twitter-auth {:twitter_auth_user (:oauth (:options arg-map))}))
+    (def twitter-auth (rdbms/get-twitter-auth {:twitter_auth_user (:oauth (:options arg-map))}))
     
-  ;; with twitter API, sometimes map keys use underscores, instead of our trusted hyphen
-  (def my-creds (make-oauth-creds (:app_consumer_key    twitter-auth)
-                                  (:app_consumer_secret twitter-auth)
-                                  (:user_access_token   twitter-auth)
-                                  (:user_access_token_secret twitter-auth)))
-  ; (println "Here is my-creds", my-creds)
-  (def num-user (rdbms/check-user {:screen_name user-name}))
-  (println "Here is num-user: ", num-user)
-  (def user-count (:count num-user))
-  (if (= user-count 0)
-    (do
-      (println "You want to create a user")
-      (actions/create-user user-name my-creds batch-time))
-    (do
-      (println "You want to get more tweets")
-      (actions/insert-more-tweets user-name my-creds batch-time)))
-  (process/create-processed-file user-name batch-time)
-  (println "Done"))
+    ;; with twitter API, sometimes map keys use underscores, instead of our trusted hyphen
+    (def my-creds (make-oauth-creds (:app_consumer_key    twitter-auth)
+                                    (:app_consumer_secret twitter-auth)
+                                    (:user_access_token   twitter-auth)
+                                    (:user_access_token_secret twitter-auth)))
+                                        ; (println "Here is my-creds", my-creds)
+    (def num-user (rdbms/check-user {:screen_name user-name}))
+    (println "Here is num-user: ", num-user)
+    (def user-count (:count num-user))
+    (if (= user-count 0)
+      (do
+        (println "You want to create a user")
+        (actions/create-user user-name my-creds batch-time))
+      (do
+        (println "You want to get more tweets")
+        (actions/insert-more-tweets user-name my-creds batch-time)))
+    (process/create-processed-file user-name batch-time)
+    )
+  (println "All done"))
 
