ebc0b3901f1779c0ff36e63366dbb55af74fd154
Eric MacAdie <eric@MacAdie.net>
2019-06-17 02:17:40 -0500

    Working on chapter 17

--- simply-clojure/src/simply_clojure/chapter17.clj
+++ simply-clojure/src/simply_clojure/chapter17.clj
@@ -20,7 +20,6 @@
         (= (max2 number (first rest-nums)) (first rest-nums)) (apply my-max rest-nums) ;; okay
         :else number))
 
-;; could this be a loop-recur?
 (defn my-max-loop-recur [the-num & rst-nums]
   (loop [number the-num
          rest-nums rst-nums]
@@ -39,9 +38,141 @@
 (defn my-max-reduce [number & rest-nums]
    (reduce max2 0 (conj rest-nums number)))
 
-(defn hello [greeting & who]
-  (println "is who a list? " (list? who))
-  (println "is who a seq?" (seq? who))
-  (println greeting who))
+;; 17.6  Implement append using car, cdr, and cons. 
+;; (Note: The built-in append can take any number of arguments. 
+;; First write a version that accepts only two arguments. 
+;; Then, optionally, try to write a version that takes any number.) 
+;; This should behave for lists and seqs like conj does for vectors.
+;; Granted, there is a lot of reversing lists. I wonder if someone has done this?
+; comp does not like macros - because the result does not carry. Got it.
+(defn nil-or-empty? [the-list]
+  (or (nil? the-list) (empty? the-list)))
+
+(defn my-append [first-list second-list]
+  (loop [list-a first-list
+         list-b second-list]
+    (cond (and (not (list? list-b)) (not (seq? list-b))) (reverse (conj (reverse list-a) list-b)) 
+          (nil-or-empty? list-b) list-a
+          (nil-or-empty? list-a) list-b
+          :else (recur (reverse (conj (reverse list-a) (first list-b))) 
+                       (rest list-b)))))
+
+(defn list-append [first-list & second-list]
+  (loop [list-a first-list
+         list-b second-list]
+    (cond (nil-or-empty? list-b) list-a
+          (nil-or-empty? list-a) (recur (first list-b) (rest list-b)) 
+          (and (= (count list-b) 1 ) (seq? (first list-b)) (empty? (first list-b))) list-a
+          :else  (recur (my-append list-a (first list-b)) (rest list-b)))))
+
+; try by converting to vectors and back
+(defn my-vec-append [first-list second-list]
+  (loop [list-a first-list
+         list-b second-list]
+    (cond (and (not (list? list-b)) (not (seq? list-b))) (seq (conj (vec list-a) list-b))
+          (nil-or-empty? list-b) list-a
+          (nil-or-empty? list-a) list-b
+          :else (recur (seq (conj (vec list-a) (first list-b))) 
+                       (rest list-b)))))
+
+(defn list-vec-append [first-list & second-list]
+  (loop [list-a first-list
+         list-b second-list]
+    (cond (nil-or-empty? list-b) list-a
+          (nil-or-empty? list-a) (recur (first list-b) (rest list-b)) 
+          (and (= (count list-b) 1 ) (seq? (first list-b)) (empty? (first list-b))) list-a
+          :else  (recur (my-vec-append list-a (first list-b)) (rest list-b)))))
+
+;; 17.7  Append may remind you of sentence. 
+;; They're similar, except that append works only with lists as arguments, whereas sentence will accept words as well as lists. 
+;; Actually, in R7RS Scheme and Racket append will take a word as the second arg
+;; Implement sentence using append. 
+;; (Note: The built-in sentence can take any number of arguments. 
+;; First write a version that accepts only two arguments. 
+;; Then, optionally, try to write a version that takes any number. 
+;; Also, you don't have to worry about the error checking that the real sentence does.) 
+;; Also from text:
+;; How does our sentence point of view differ from the built-in Scheme point of view using lists? There are three differences:
+;; A sentence can contain only words, not sublists.
+;; Sentence selectors are symmetrical front-to-back.
+;; Sentences and words have the same selectors.
+;; I will still check that if it's a list it does not have a sublist
+  
+;; I will still check that if it's a list it does not have a sublist
+; (set! *default-data-reader-fn* tagged-literal)
+
+(defn list-with-sublists? [the-list]
+  (cond (not (list? the-list)) false
+        (= 0 (count (filter list? the-list))) false
+        :else true)) 
+
+(defn list-or-item [the-item]
+  (cond (not (list? the-item)) (list the-item)
+        (list-with-sublists? the-item) nil
+        :else the-item))
+
+(defn sentence-via-append [list-a list-b]
+  (my-append (list-or-item list-a) (list-or-item list-b)))
+
+(defn multi-sentence-append [first-list & second-list]
+  (loop [list-a first-list
+         list-b second-list]
+    (cond (nil-or-empty? list-b) (remove nil? list-a)    
+          (nil? list-a) (recur (first list-b) (rest list-b)) 
+          ;; Racket got empty list in a list '(()) - might not need this for clojure
+          ; (and (= 1 (count list-b)) (list? (first list-b)) (empty? (first list-b))) list-a 
+          :else (recur (sentence-via-append list-a (first list-b)) (rest list-b))))) 
+
+; once again, winnin' with the 'duce!!
+(defn multi-sentence-reduce [list-a & list-b]
+  (remove nil? (sentence-via-append list-a (reduce sentence-via-append list-b))))
+
+;; 17.8  Write member.
+;; I don't think you can use higher-order functions, because I think you have to go through the entire collection with HOF
+(defn new-member [the-item the-list]
+  (loop [item-a the-item
+         list-a the-list]
+      (cond (nil-or-empty? list-a) false
+            (= item-a (first list-a)) list-a
+            :else (recur item-a (rest list-a)))))
+
+;; 17.9  Write list-ref. 
+;; The list equivalent of item is called list-ref (short for "reference")
+; it's different in that it counts items from zero instead of from one and takes its arguments in the other order
+;; okay, I looked at the other solutions, and yes, decrementing is more efficient
+(defn my-list-ref [the-list the-num]
+  (loop [list-a the-list
+         num-a the-num]
+    (cond (nil-or-empty? list-a) false
+          (= num-a 0) (first list-a)
+          :else (recur (rest list-a) (dec num-a)))))
+
+;; 17.10  Write length. 
+(defn my-length [the-list]
+  (loop [list-a the-list
+         counter 0] ;; no need for variadic w/loop recur
+    (cond (nil-or-empty? list-a) counter 
+          :else (recur (rest list-a) (inc counter)))))
+
+;; 17.11  Write before-in-list?, which takes a list and two elements of the list. 
+;; It should return #t if the second argument appears in the list argument before the third argument:
+; > (before-in-list? '(back in the ussr) 'in 'ussr) returns #T
+; > (before-in-list? '(back in the ussr) 'the 'back) returns #F
+;; let's compare the length of our members
+(defn before-in-list? [list-a item-a item-b]
+  (cond (or (nil? item-a) (nil? item-b)) false
+        (or (not (new-member item-a list-a)) (not (new-member item-b list-a))) false
+        ; earlier item will have longer list from member
+        (> (my-length (new-member item-a list-a))
+           (my-length (new-member item-b list-a))) true
+        :else false))
+
+
+(defn greet [name & rest-n]
+  (println "is rest-n a list: " (list? rest-n))
+  (println "is rest-n a seq: " (seq? rest-n))
+  (println "Hello" name rest-n))
+
+
 
 
--- simply-clojure/test/simply_clojure/chapter17_test.clj
+++ simply-clojure/test/simply_clojure/chapter17_test.clj
@@ -27,6 +27,108 @@
     (test/is (= 4 (my-max-reduce 4 3 4 2 1)))
     (test/is (= 5 (my-max-reduce 4 3 5 2 1)))))
 
+(test/deftest test-my-append
+  (test/testing "Testing my-append"
+    (test/is (= '(yellow sub the word) (my-append '(yellow sub) '(the word))))
+    (test/is (= '(yellow sub the word) (my-append '(yellow sub) (seq '(the word)))))
+    (test/is (= '(yellow sub the word) (my-append (seq '(yellow sub)) (seq '(the word)))))
+    (test/is (= '(yellow sub the word) (my-append (seq '(yellow sub)) '(the word))))
+    (test/is (= '(i am the walrus) (my-append '(i am) '(the walrus))))
+    (test/is (= '(Rod Argent Chris White) (my-append '(Rod Argent) '(Chris White))))
+    ;; from Husk R7RS docs
+    (test/is (= '(x y) (my-append '(x) '(y))))
+    (test/is (= '(a b c d) (my-append '(a) '(b c d))))
+    (test/is (= '(a (b) (c)) (my-append '(a (b)) '((c)))))
+    (test/is (= '(a) (my-append '() 'a)))
+    (test/is (= '(1 2 3 4 5 '(10 11 12)) (my-append '(1 2 3) '(4 5 '(10 11 12)))))))
+
+(test/deftest test-list-append
+  (test/testing "Testing list-append"
+    (test/is (= '(1 2 3 4 5 6 7 8 9) (list-append '(1 2 3) '(4 5 6) '(7 8 9))))
+    (test/is (= '(1 2 3 4) (list-append '(1 2 3) 4)))
+    (test/is (= '(1 2 3 4 5 6 7 8 9) (list-append '(1 2 3) 4 5 6 '(7 8 9))))
+    (test/is (= '(1 2 3 4 5 6 7 8 9) (list-append '(1 2 3) '(4 5 6) 7 8 9)))))
+
+(test/deftest test-my-vec-append
+  (test/testing "Testing my-vec-append"
+    (test/is (= '(yellow sub the word) (my-vec-append '(yellow sub) '(the word))))
+    (test/is (= '(yellow sub the word) (my-vec-append '(yellow sub) (seq '(the word)))))
+    (test/is (= '(yellow sub the word) (my-vec-append (seq '(yellow sub)) (seq '(the word)))))
+    (test/is (= '(yellow sub the word) (my-vec-append (seq '(yellow sub)) '(the word))))
+    (test/is (= '(i am the walrus) (my-vec-append '(i am) '(the walrus))))
+    (test/is (= '(Rod Argent Chris White) (my-vec-append '(Rod Argent) '(Chris White))))
+    ;; from Husk R7RS docs
+    (test/is (= '(x y) (my-vec-append '(x) '(y))))
+    (test/is (= '(a b c d) (my-vec-append '(a) '(b c d))))
+    (test/is (= '(a (b) (c)) (my-vec-append '(a (b)) '((c)))))
+    (test/is (= '(a) (my-vec-append '() 'a)))
+    (test/is (= '(1 2 3 4 5 '(10 11 12)) (my-vec-append '(1 2 3) '(4 5 '(10 11 12)))))))
+
+(test/deftest test-list-vec-append
+  (test/testing "Testing list-vec-append"
+    (test/is (= '(1 2 3 4 5 6 7 8 9) (list-vec-append '(1 2 3) '(4 5 6) '(7 8 9))))
+    (test/is (= '(1 2 3 4) (list-vec-append '(1 2 3) 4)))
+    (test/is (= '(1 2 3 4 5 6 7 8 9) (list-vec-append '(1 2 3) 4 5 6 '(7 8 9))))
+    (test/is (= '(1 2 3 4 5 6 7 8 9) (list-vec-append '(1 2 3) '(4 5 6) 7 8 9)))))
+
+(test/deftest test-sentence-via-append
+  (test/testing "Testing sentence-via-append"
+    (test/is (= '(hello world) (sentence-via-append 'hello 'world)))
+    (test/is (= '(hello this is a ren) (sentence-via-append 'hello '(this is a ren))))
+    (test/is (= '(what is this this is a ren) (sentence-via-append '(what is this) '(this is a ren))))
+    (test/is (= '(what is this re) (sentence-via-append '(what is this) 're)))
+    (test/is (= '(what is this re) (sentence-via-append '(what is this) (list 're))))
+    (test/is (= '(hello nil) (sentence-via-append 'hello '(this is a '(deep list) bro))))
+    (test/is (= '(hello) (sentence-via-append '(this is a '(deep list) bro) 'hello)))
+    (test/is (= '(hello) (sentence-via-append 'hello '())))
+    (test/is (= '(hello) (sentence-via-append '() 'hello)))
+    (test/is (= '(hello 4) (sentence-via-append 'hello 4)))
+    (test/is (= '(hello 4) (sentence-via-append 'hello '(4))))))
+
+(test/deftest testing-multi-sentence-append
+  (test/testing "Testing multi-sentence-append"
+    (test/is (= '(hello there bug guy) (multi-sentence-append 'hello 'there 'bug 'guy)))
+    (test/is (= '(hello there bug guy) (multi-sentence-append 'hello 'there '(bug guy))))
+    (test/is (= '(hello there bug guy) (multi-sentence-append 'hello '(there bug) 'guy)))
+    (test/is (= '(hello there bug guy) (multi-sentence-append '(hello there) 'bug 'guy)))
+    (test/is (= '(hello world this is a list okay) (multi-sentence-append 'hello 'world '(this is a list) 'okay '(this is a '(deep) list)) ))
+    (test/is (= '(hello world this is a list okay this is fine) (multi-sentence-append 'hello 'world '(this is a list) 'okay '(this is a '(deep) list) '(this is fine))))))
+
+(test/deftest testing-multi-sentence-reduce
+  (test/testing "Testing multi-sentence-reduce"
+    (test/is (= '(hello there bug guy) (multi-sentence-reduce 'hello 'there 'bug 'guy)))
+    (test/is (= '(hello there bug guy) (multi-sentence-reduce 'hello 'there '(bug guy))))
+    (test/is (= '(hello there bug guy) (multi-sentence-reduce 'hello '(there bug) 'guy)))
+    (test/is (= '(hello there bug guy) (multi-sentence-reduce '(hello there) 'bug 'guy)))
+    (test/is (= '(hello world this is a list okay) (multi-sentence-reduce 'hello 'world '(this is a list) 'okay '(this is a '(deep) list)) ))
+    (test/is (= '(hello world this is a list okay this is fine) (multi-sentence-reduce 'hello 'world '(this is a list) 'okay '(this is a '(deep) list) '(this is fine))))))
+
+(test/deftest test-new-member
+  (test/testing "Testing new-member"
+    (test/is (= '(d e f g) (new-member 'd '(a b c d e f g))))
+    (test/is (= '(e f g) (new-member 'e '(a b c d e f g))))
+    (test/is (= false (new-member 'h '(a b c d e f g))))
+    (test/is (= false (new-member 4 '(a b c d e f g))))))
+
+(test/deftest test-my-list-ref
+  (test/testing "Testing my-list-ref"
+    (test/is (= 'best (my-list-ref '(Lisp is the best language) 3)))
+    (test/is (= 'c (my-list-ref '(a b c d) 2)))
+    (test/is (= false (my-list-ref '(a b c d) 5)))
+    (test/is (= false (my-list-ref '(a b c d) -5)))))
+
+(test/deftest test-my-length
+  (test/testing "Testing my-length"
+    (test/is (= 3 (count '(a b c)) (my-length '(a b c)))) 
+    (test/is (= 3 (count '(a (b) (c d e)))  (my-length '(a (b) (c d e)))))
+    (test/is (= 3 (count '(a (b) (c d '(f g h) e)))  (my-length '(a (b) (c d '(f g h) e)))))
+    (test/is (= 0 (count '()) (my-length '())))))
+
+(test/deftest test-before-in-list?
+  (test/testing "Testing before-in-list"
+    (test/is (= true (before-in-list? '(back in the ussr) 'in 'ussr)))
+    (test/is (= false (before-in-list? '(back in the ussr) 'the 'back)))
+    (test/is (= false (before-in-list? '(back in the ussr) 'in 'usa)))))
 
 
 
